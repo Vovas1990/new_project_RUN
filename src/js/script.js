0088f0c3998d647e7de95100142dd611daade95b
vovas <vovas234pp@gmail.com>
2022-01-07 19:21:27 +0300

    yrok_js

---
+++ src/js/script.js
@@ -0,0 +1,52 @@
+/* var name = "iman";
+let number = 4;         
+const pi = 3.14; 
+alert ('iman')
+
+console.log(number); */
+
+
+/* let answer = confirm('who?');
+console.log(answer) */
+
+
+/* let answer = prompt('who?', "");
+console.log(answer)
+
+console.log(4+"pi") */
+
+/* if (2*4 == 8*1){
+  console.log('верно')
+} else {
+  console.log('не верно')
+}
+ */
+
+/* let answer = confirm('who 18?');
+if (answer) {
+  console.log('проходите') 
+} else {
+  console.log('уходи') */
+
+/* const num = 49;
+
+if (num < 49) {
+  console.log('нет')
+
+}else if (num > 100) {
+    console.log('много')  
+} else {
+    console.log('хватит')
+} */
+
+
+
+/* for(let i = 1; i < 8; i++) {
+  console.log(i)
+} */
+
+/* function logi(a, b) {
+  console.log(a + b)
+}
+
+logi(3, 6); */
