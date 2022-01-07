97dc5f189bda208235b0c492f886e203be08ded5
vovas <vovas234pp@gmail.com>
2022-01-07 20:47:03 +0300

    slaide

--- src/js/script.js
+++ src/js/script.js
@@ -1,52 +1,21 @@
-/* var name = "iman";
-let number = 4;         
-const pi = 3.14; 
-alert ('iman')
+$(document).ready(function(){
+  $('.carusel__inner').slick({    
+    speed: 1000,    
+    adaptiveHeight: true,
+    /* autoplay: true,
+    autoplaySpeed: 2000 */
+    prevArrow: '<button type="button" class="slick-prev"><img src="icon/block-4/chevron-left-solid.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icon/block-4/chevron-right-solid.svg"></button>',
+    responsive: [
+       {
+        breakpoint: 768,
+        settings: {          
+          dots: true,
+          arrows: false
+        }
+      } 
+      
+    ]
+  });
+});
 
-console.log(number); */
-
-
-/* let answer = confirm('who?');
-console.log(answer) */
-
-
-/* let answer = prompt('who?', "");
-console.log(answer)
-
-console.log(4+"pi") */
-
-/* if (2*4 == 8*1){
-  console.log('верно')
-} else {
-  console.log('не верно')
-}
- */
-
-/* let answer = confirm('who 18?');
-if (answer) {
-  console.log('проходите') 
-} else {
-  console.log('уходи') */
-
-/* const num = 49;
-
-if (num < 49) {
-  console.log('нет')
-
-}else if (num > 100) {
-    console.log('много')  
-} else {
-    console.log('хватит')
-} */
-
-
-
-/* for(let i = 1; i < 8; i++) {
-  console.log(i)
-} */
-
-/* function logi(a, b) {
-  console.log(a + b)
-}
-
-logi(3, 6); */
